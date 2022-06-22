c0c261afa244594e348e7b20adea90e4ffd1deb5
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-22 10:34:30 +0300

    api integration

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
-(function () {
-    window.addEventListener('DOMContentLoad', () =>{
-        console.log("Loaded");
-    });
+(function(){
+    //variable
+     let demo = document.getElementById("demo");
+    //events
+    window.addEventListener('DOMContentLoaded', () => {
+        //document.querySelector("#demo")
+        console.log("connected");
+        fetchData(); //start fetch data
+    })
+    //method
+    
+    //fetch data --api
+    function fetchData(){
+        fetch (`https://api.json-generator.com/templates/dkGUFM9yecgO/data?access_token=8ziyqp1pbfk3vid625oroxgvsh5yzzb7m9lqcadk`)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+            data.forEach( elem => {
+                console.log(elem.recipes);
+                recipes(elem.recipes)
+            });
+            
+        })
+        .catch(err => console.error(err));
+    }
+    //create recipe component and update on dom
+    function recipes(recipes) {
+        let output = '';
+        //map the output
+        recipes.map(recipe => {
+            output += `
+                    
+                    <div class  = "recipe">
+                        <img src = ${recipe.img} alt="recipe" width="300px">
+                        <h5> ${recipe.title} </h5>
+                        <p> ${recipe.desc} </p>
+                        <div class =ctrls>
+                            <span class = "like" id = "like"> ${recipe.likes}:likes </span>
+                            <i class="fal fa-heart"> ${recipe.likes} </i>
+                            <i class="fal fa-comment"> ${recipe.comment} </i>
+                            <input type="button" value ="read more" id="read-btn" />
+                        </div>
+                    </div>
+                   
+                `;
 
-})
+        });
+        //update dom output
+       return demo.innerHTML = output;
+    }
+    
+})();
